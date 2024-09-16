b788851c374c357cccb7de44edf70e4e17a28f6b
Oscar Ivan Sierra Gaona <oscar00gaona@gmail.com>
2024-09-16 15:33:56 -0500

    Botones agregados

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,22 @@ addTarea.addEventListener('submit', (event)=>{
     const ingresarTarea = document.getElementById('newTarea');
     const informacionTarea = ingresarTarea.value;
 
-    agregarTarea(informacionTarea);
+    
+    if (editarIndex != null) {
+        tareas[editarIndex].informacionTarea = informacionTarea;
+        editarIndex = null;
+        addTarea.querySelector('button').textContent = 'Agregar'
+    }else{
+        agregarTarea(informacionTarea);
+    }
     ingresarTarea.value = ''; 
+    mostrarTareas();
 
 });
 
 
 let tareas = [];
+let editarIndex = null;
 
 function agregarTarea(informacionTarea){
     tareas.push({
@@ -28,9 +37,50 @@ function mostrarTareas() {
     const listaTareas= document.getElementById('listaTareas');
     listaTareas.innerHTML = '';
 
-    tareas.forEach(tarea => {
+    tareas.forEach((tarea, index) => {
         const itemTarea = document.createElement('li');
         itemTarea.textContent = `${tarea.informacionTarea} - ${tarea.estado? 'Realizada' : 'Pendiente'}`;
         listaTareas.appendChild(itemTarea);
+
+        //boton para editasr la tarea
+        const btnEditar = document.createElement('button');
+        btnEditar.textContent = 'Editar';
+        btnEditar.classList.add('btn'); 
+        btnEditar.onclick = () => editarTarea(index)
+        itemTarea.appendChild(btnEditar)
+
+        //eliminar tarea
+        const btnEliminar = document.createElement('button');
+        btnEliminar.textContent = 'Eliminar';
+        btnEliminar.classList.add('btnDelete');
+        btnEliminar.onclick = () => eliminarTarea(index)
+        itemTarea.appendChild(btnEliminar)
+
+        //Cambiar de estado la tarea
+        const btnEstado = document.createElement('button');
+        btnEstado.textContent = 'Cambiar Estado';
+        btnEstado.classList.add('btnEstado');
+        btnEstado.onclick = () => cambiarEstado(index)
+        itemTarea.appendChild(btnEstado)
+
     });
+}
+
+function editarTarea(index) {
+    const editarTarea = document.getElementById('newTarea');
+    editarTarea.value = tareas[index].informacionTarea;
+
+    editarIndex = index;
+    addTarea.querySelector('button').textContent = 'Guardar tarea';
+    
+}
+
+function eliminarTarea(index) {
+    tareas.splice(index,1);
+    mostrarTareas(); 
+}
+
+function cambiarEstado(index) {
+    tareas[index].estado = !tareas[index].estado
+    mostrarTareas();
 }
